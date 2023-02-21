4ef6cb81f721b489082f7eaa67863de20faa41ce
ozarskiw <ozarskiw@gmail.com>
2023-02-21 12:33:40 +0100

    App crash in light mode theme bugfix

--- app/src/main/java/com/example/goaltracker/MainActivity.kt
+++ app/src/main/java/com/example/goaltracker/MainActivity.kt
@@ -3,15 +3,9 @@ package com.example.goaltracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
-import com.example.goaltracker.database.GoalDatabase
-import com.example.goaltracker.database.SessionDatabaseService
-import com.example.goaltracker.database.TimeGoalDatabaseService
 import com.example.goaltracker.databinding.ActivityMainBinding
-import com.example.goaltracker.goal.TimeGoal
-import com.example.goaltracker.goal.getTimeDebt
 import com.example.goaltracker.mainActivityTabs.SectionAdapterMain
 import com.google.android.material.tabs.TabLayout
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
